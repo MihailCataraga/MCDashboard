a91a5f4dc00bf6672b14cd8f0dcc0df8bd46bd46
Mihai <cataraga.95@gmail.com>
2024-01-03 15:10:37 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, HashRouter } from "react-router-dom";
+import Biling from "./pages/Billing";
+import Home from "./pages/Home";
+import Settings from "./pages/Settings";
+import Tables from "./pages/Tables";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tables" element={<Tables />} />
+        <Route path="/billing" element={<Biling />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
